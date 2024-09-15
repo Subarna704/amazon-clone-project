6ed10e6760f882f276a0f8b313c42a8e3d252538
subarna704 <subarnashrestha704@gmail.com>
2024-09-15 21:27:19 +1000

    Generating order-summary usning js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,15 @@
-export const cart =[];
+export const cart =[{
+    productId: "e43638ce-6aa0-4b85e1d07eb678c6",
+    quantity: 2
+    },
+{   
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+    
+}, {
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    quantity: 2
+}];
 
 export function addToCart(productId){
     let selects = document.querySelectorAll(`.js-quantity-selector-${productId}`);
